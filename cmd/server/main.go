705349b286e6ca9c9bf5dba1aef985c79e38b4e6
Miki Tebeka <miki@353solutions.com>
2023-03-05 13:46:21 +0200

    03_03b

---
+++ cmd/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/353solutions/rides/pb"
+)
+
+func main() {
+	addr := ":9292"
+
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("error: can't listen - %s", err)
+	}
+
+	srv := grpc.NewServer()
+	var u Rides
+	pb.RegisterRidesServer(srv, &u)
+
+	log.Printf("info: server ready on %s", addr)
+	if err := srv.Serve(lis); err != nil {
+		log.Fatalf("error: can't serve - %s", err)
+	}
+}
+
+type Rides struct {
+	pb.UnimplementedRidesServer
+}

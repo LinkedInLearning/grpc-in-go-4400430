e6439573f59dfd57fa80124b7e87a68f70146024
Miki Tebeka <miki@353solutions.com>
2023-03-05 13:46:22 +0200

    04_02

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,8 +7,10 @@ import (
 	"time"
 
 	"github.com/353solutions/rides/pb"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func main() {
@@ -30,5 +32,24 @@ func main() {
 
 	log.Printf("info: connected to %s", addr)
 	c := pb.NewRidesClient(conn)
-	fmt.Println(c)
+
+	req := pb.StartRequest{
+		Id:       "47a74960d6204a52b1bece53221eb458",
+		DriverId: "007",
+		Location: &pb.Location{
+			Lat: 51.4871871,
+			Lng: -0.1266743,
+		},
+		PassengerIds: []string{"M", "Q"},
+		Time:         timestamppb.Now(),
+		Type:         pb.RideType_POOL,
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	resp, err := c.Start(ctx, &req)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	fmt.Println(resp)
 }
